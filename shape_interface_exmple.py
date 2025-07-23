3202c8e2b3bc43f7377cf4c5b156ebb5ba7aa440
jamiganesh <ganeshjami8@gmail.com>
2025-07-23 19:03:33 +0530

    added for square

--- shape_interface_exmple.py
+++ shape_interface_exmple.py
@@ -21,6 +21,22 @@ class Rectangle(Shape):
     def perimeter(self):
         return 2*(self.l + self.w)
     
+class Square(Shape):
+    def __init__(self,side):
+        self.side = side 
+
+    def area(self):
+        return self.side * self.side 
+    
+    def perimeter(self):
+        return self.side * 4
+               
+
+
+s1 = Square(6)
+print(s1.area())
+print(s1.perimeter())
+
 
 r1 = Rectangle(4,2) 
 print(r1.area())    
